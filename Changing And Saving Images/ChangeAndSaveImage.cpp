c0c095747d630b5f97df4eed1b0b0aee20e4f2d3
ShadyD45 <shubhamdhumal732@gmail.com>
2020-09-08 12:28:10 +0530

    added basic change and save orginal image to grayscale script

--- Changing And Saving Images/ChangeAndSaveImage.cpp
+++ Changing And Saving Images/ChangeAndSaveImage.cpp
@@ -1,31 +1,13 @@
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
-using namespace std;
 
 int main(int argc, char** argv )
 {
-    // Error Handling
-    // Check if image path is passed to the program
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-   
     // Read the image using imread()
     Mat mOriginal = imread( argv[1], IMREAD_COLOR);
     //Read image in grayscale
 	Mat mModified = imread(argv[1], IMREAD_GRAYSCALE); 
-	
-    
-    // Error Handling
-    // Checking if the read image has data or not
-    if ( !mOriginal.data || !mModified.data)
-    {
-        printf("No image data \n");
-        return -1;
-    }
     
     // Create windows for showing Original and Modified images
     namedWindow("Original Image", WINDOW_AUTOSIZE );
