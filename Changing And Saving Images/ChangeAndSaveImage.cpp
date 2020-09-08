dbe4eb84517ebf4aa5f6baf04eb52398ebca8d85
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2020-09-08 12:32:36 +0530

    save orignal image to grayscale basic program

--- Changing And Saving Images/ChangeAndSaveImage.cpp
+++ Changing And Saving Images/ChangeAndSaveImage.cpp
@@ -4,23 +4,23 @@ using namespace cv;
 
 int main(int argc, char** argv )
 {
-    // Read the image using imread()
-    Mat mOriginal = imread( argv[1], IMREAD_COLOR);
-    //Read image in grayscale
-	Mat mModified = imread(argv[1], IMREAD_GRAYSCALE); 
-    
-    // Create windows for showing Original and Modified images
-    namedWindow("Original Image", WINDOW_AUTOSIZE );
-    namedWindow("Modified Image", WINDOW_AUTOSIZE);
-    
-    // Show Original and Modified Image on the created windows(Just for comaparison)
-    imshow("Original Image", mOriginal);
-    imshow("Modified Image", mModified);
-    
-    // Wait forever until key press
-	waitKey(0);
-	
-	// Write the grayscale image
-	imwrite("GrayImg.jpg", mModified);
-    return 0;
+		// Read the image using imread()
+		Mat mOriginal = imread( argv[1], IMREAD_COLOR);
+		//Read image in grayscale
+		Mat mModified = imread(argv[1], IMREAD_GRAYSCALE); 
+
+		// Create windows for showing Original and Modified images
+		namedWindow("Original Image", WINDOW_AUTOSIZE );
+		namedWindow("Modified Image", WINDOW_AUTOSIZE);
+
+		// Show Original and Modified Image on the created windows(Just for comaparison)
+		imshow("Original Image", mOriginal);
+		imshow("Modified Image", mModified);
+
+		// Wait forever until key press
+		waitKey(0);
+
+		// Write the grayscale image
+		imwrite("GrayImg.jpg", mModified);
+		return 0;
 }
